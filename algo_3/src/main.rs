627a63ea3618cae6a3ecbe6ce47de6d9908b4199
sadagatasgarov <sadagat.asgarov@gmail.com>
2024-05-15 00:06:22 +0400

    algo_3 Sorted_Squared Arrayde menfi eded oanda siralam sehv olurdu dezeltdim

--- algo_3/src/main.rs
+++ algo_3/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let mut v = vec![1,2,3,4,5,6,8,10,9];
+    let mut v = vec![1,-8,2,3,4,5,6,8,10,9];
     let b = sorted_squared_array(v.clone());
 
     println!("{:?}", v);
@@ -9,12 +9,12 @@ fn main() {
 
 
 
-//menim hell yolum derse baxiram gorek orada nece olur
-fn sorted_squared_array(mut array: Vec<i32>) ->Vec<i32> {
-    array.sort();
+//menim hell yolum derse baxiram gorek orada nece olur 
+fn sorted_squared_array(array: Vec<i32>) ->Vec<i32> {
     let mut n = vec![];
     for i in array.iter() {
         n.push(i*i)
     }
+    n.sort();
     n
 }
