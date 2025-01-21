4631f6c92a989825c34df993c27e1de7cb772307
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2025-01-21 14:46:31 +0400

    Bt qurdum

--- algo_9_nd/src/main.rs
+++ algo_9_nd/src/main.rs
@@ -1,3 +1,26 @@
+
+
+
+struct BinaryTree {
+    value: i32,
+    left_node: Option<Box<BinaryTree>>,
+    right_node: Option<Box<BinaryTree>>
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let bt = BinaryTree{
+        value:1,
+        left_node: Some(Box::new(BinaryTree{
+            value: 2,
+            left_node: None,
+            right_node: Some(Box::new(BinaryTree{
+                value: 3,
+                left_node: None,
+                right_node: None,
+            }))
+        })),
+        right_node:None
+    };
+
 }
