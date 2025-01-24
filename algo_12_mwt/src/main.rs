37284d2867a58435ddd6269cd094b7cc24c2506b
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2025-01-24 13:22:52 +0400

    minimum waiting time

--- algo_12_mwt/src/main.rs
+++ algo_12_mwt/src/main.rs
@@ -1,23 +1,20 @@
-
 // O(nlogn) time | O(1) space
 fn minimum_waiting_time(mut query: Vec<usize>) -> usize {
-    query.sort();
-    let mut total_waiting_time = 0;
+    query.sort(); // Diziyi küçükten büyüğe sırala
+    let mut total_waiting_time = 0; // Toplam bekleme süresi
 
-    for (idx, duration) in query.iter().enumerate(){
-        let qleft = query.len() - (idx);
-        total_waiting_time += duration * qleft;      //println!("{}, {:?}, {}", qleft, duration, total_waiting_time)
+    for (idx, duration) in query.iter().enumerate() {
+        let remaining_queries = query.len() - (idx + 1); // Geriye kalan işlemler
+        total_waiting_time += duration * remaining_queries; // Bekleme süresine katkı ekle
     }
 
     total_waiting_time
 }
-
-
 
 fn main() {
     let v = vec![3, 2, 1, 8];
 
     let mwt = minimum_waiting_time(v);
 
-    println!("{}", mwt);
+    println!("Minimum Waiting Time: {}", mwt); // Çıktı: 10
 }
